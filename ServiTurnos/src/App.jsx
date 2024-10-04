fbec75edcd3f4a3d3a8d3a10647bc74afa40ef25
Cristian Ninotti <cristianninotti03@gmail.com>
2024-10-03 22:19:41 -0300

    Routes en componente

--- ServiTurnos/src/App.jsx
+++ ServiTurnos/src/App.jsx
@@ -1,20 +1,19 @@
 import './App.css';
-import WelcomePage from './components/welcomePage/WelcomePage';
-import Register from './components/register/Register';
-
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import RoutesComponent from './RoutesComponent';
 //element es donde especificamos que componente debe renderizar, no hace falta props
 //porque lo estamos pasando mediante una ruta = Route
+
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<WelcomePage />} /> 
-        
-        <Route path="/register" element={<Register />} />
-      </Routes>
-    </Router>
+    <div>
+      <RoutesComponent />
+    </div>
   );
 };
 
 export default App;
+
+
+
+
+
