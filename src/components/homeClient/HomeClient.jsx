106aac3c789292e7258f03db711473cf94226d55
Nahuel González <nahuelgonzalez775@gmail.com>
2024-10-10 21:16:50 -0300

    update

--- src/components/homeClient/HomeClient.jsx
+++ src/components/homeClient/HomeClient.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { Form, Button, Row, Col } from 'react-bootstrap';
+
 //import './Perfil.css'; // Aquí agregarías los estilos personalizados.
 
 const HomeClient = () => {
